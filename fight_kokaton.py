9bc014c2cc3e7d1ba2e75db528d5ffdc56cd8359
c0a24219da <c0a24219da@edu.teu.ac.jp>
2025-10-07 14:28:46 +0900

    練習３：こうかとん喜びエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -172,6 +172,7 @@ def main():
                     # ビームと爆弾の衝突判定
                     beam = None
                     bomb = None
+                    bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
